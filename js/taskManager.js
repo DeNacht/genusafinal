d8fde32660f3c72df2f2242bb4d5c47610d69057
ametelwokil2472 <ametelwokil@gmail.com>
2021-02-04 14:12:17 -0600

    amet

--- js/taskManager.js
+++ js/taskManager.js
@@ -2,18 +2,48 @@ class TaskManager {
   constructor() {
     this.task = [];
     this.currentId = 0;
+  //  this.name =  '';
+  //   this.description ='';
+  //  this.assignedTo ='';
+  //   this.dueDate = Date;
+  //   this.status = ''
   }
-  addTask(name, description, assignedTo, dueDate, status) {
+  getCurrentID (){
+   // this.currentId = 0;
     this.currentId++;
+  } 
+  addTask(name, description, assignedTo, dueDate, status) {
+    
+    this.name =  name;
+    this.description = description;
+   this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status
+       
   }
-}
-const newTask = new TaskManager("newTask");
-const newAdded = newTask.addTask(
+
+ }
+let newTask = new TaskManager('myTask');
+myTask = newTask.addTask(
+  newTask.getCurrentID(),
   "Developer",
   "nice work",
   "Tigist",
-  2021 / 10 / 02,
+  Date,
   "ToDo"
 );
-const last = newTask.push(newAdded);
+newTask.task.push(myTask);
 console.log(newTask);
+
+let addTask = new TaskManager('todo')
+todo = addTask.addTask(
+  addTask.getCurrentID(),
+  "group5",
+  "good",
+  "Addis",
+  2021/02/23,
+  "ToDo"
+);
+addTask.task.push(todo)
+console.log(addTask);
+console.log(addTask.task.length)
