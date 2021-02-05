15566b7053e719ff38cb807a146414bce5ccf056
DeNacht <denise.nactigal@gmail.com>
2021-02-04 23:33:37 -0600

    tasks increment

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,49 +1,24 @@
 class TaskManager {
-  constructor() {
+  constructor(currentId = 0) {
     this.task = [];
-    this.currentId = 0;
-  //  this.name =  '';
-  //   this.description ='';
-  //  this.assignedTo ='';
-  //   this.dueDate = Date;
-  //   this.status = ''
+    this.currentId = currentId; 
   }
-  getCurrentID (){
-   // this.currentId = 0;
-    this.currentId++;
-  } 
-  addTask(name, description, assignedTo, dueDate, status) {
-    
-    this.name =  name;
-    this.description = description;
-   this.assignedTo = assignedTo;
-    this.dueDate = dueDate;
-    this.status = status
-       
+  addTask(taskName, desc, assign, due, stat) {
+    this.currentId++;    
+    const jobName = {
+      name: taskName,
+      description: desc,
+      assignedTo: assign,
+      dueDuate: due,
+      status: stat
+    } 
+    this.task.push(jobName);
   }
+}
+const taskArray = new TaskManager ();
 
- }
-let newTask = new TaskManager('myTask');
-myTask = newTask.addTask(
-  newTask.getCurrentID(),
-  "Developer",
-  "nice work",
-  "Tigist",
-  Date,
-  "ToDo"
-);
-newTask.task.push(myTask);
-console.log(newTask);
+taskArray.addTask('final', 'project', 'denise', 'today', 'TODO')
+taskArray.addTask('final', 'project', 'denise', 'today', 'TODO')
+taskArray.addTask('final', 'project', 'denise', 'today', 'TODO')
 
-let addTask = new TaskManager('todo')
-todo = addTask.addTask(
-  addTask.getCurrentID(),
-  "group5",
-  "good",
-  "Addis",
-  2021/02/23,
-  "ToDo"
-);
-addTask.task.push(todo)
-console.log(addTask);
-console.log(addTask.task.length)
+console.log(taskArray);
